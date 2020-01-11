188441f28cafbefb5104df82f22b2cbb9bf88061
Alikhan Batchaev <loko-han@mail.ru>
2020-01-11 15:59:04 +0300

    Initial Commit

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by 🧔🏻Alikhan Batchaev on 11.01.2020.
+//  Copyright © 2020 Alikhan Batchaev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
