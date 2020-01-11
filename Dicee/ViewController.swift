0807e7033667bf909b249dd7420ba3f00541ba02
Alikhan Batchaev <loko-han@mail.ru>
2020-01-11 18:40:20 +0300

    добавлен массив вариаций выпавших кубиков

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -9,12 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var diceImageViewOne: UIImageView!
+    @IBOutlet var diceImageViewTwo: UIImageView!
+    
+    var leftDiceNumber = 1
+    var rightDiceNumber = 5
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        diceImageViewOne.alpha = 0.5
+        diceImageViewTwo.alpha = 0.5
     }
-
-
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        diceImageViewOne.alpha = 1
+        diceImageViewTwo.alpha = 1
+        
+        diceImageViewOne.image =  [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")] [leftDiceNumber]
+        diceImageViewTwo.image =  [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")] [rightDiceNumber]
+        
+        leftDiceNumber += 1
+        rightDiceNumber -= 1
+    }
+    
 }
 
