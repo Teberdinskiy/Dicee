992a59c1342ba2300d43fa63b0fde3f8af0d43dd
Alikhan Batchaev <loko-han@mail.ru>
2020-01-12 18:08:17 +0300

    рефакторинг добавил комментарии

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -13,12 +13,12 @@ class ViewController: UIViewController {
     @IBOutlet var diceImageViewOne: UIImageView!
     @IBOutlet var diceImageViewTwo: UIImageView!
     
-    let dice = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+    let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        // dim dice on first launch
         diceImageViewOne.alpha = 0.5
         diceImageViewTwo.alpha = 0.5
     }
@@ -28,10 +28,10 @@ class ViewController: UIViewController {
         diceImageViewOne.alpha = 1
         diceImageViewTwo.alpha = 1
         
-        diceImageViewOne.image = dice [Int.random(in: 0...5)]
-        diceImageViewTwo.image =  dice [Int.random(in: 0...5)]
+        diceImageViewOne.image = diceArray [Int.random(in: 0...5)]
+        // as an option, you can use a random range inside the array
+        diceImageViewTwo.image = diceArray.randomElement()
         
     }
-    
 }
 
